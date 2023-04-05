85de3ee563cc6fed741ac74a8a4156a98fd88a69
Neha Singh <neha.singh931126@gmail.com>
2023-04-05 13:22:17 +0200

    Added the gravatar

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const db = config.get("mongoURI");
 const connectDB = async ()=> {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser: true
+            useNewUrlParser: true, // if you get the deprecated warngin
+            useCreateIndex: true
         });
         console.log("mongdb connected");
     } catch(error) {
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require('gravatar');
 const { check, validationResult } = require("express-validator");
+const User = require('../../model/User');
 
 // @route   GET api/users
 // @desc    Test route
@@ -18,23 +20,37 @@ router.post(
     check("email", "Please include a valid email").isEmail(),
     check("password", "Please enter a password with 6 or more charaters").isLength(6),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // see if users exists
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({
+          errors: [{ msg: "User already exists" }],
+        });
+      }
+      res.send("user route");
+    } catch (error) {
+      console.log(error);
+      res.status(500).send("server error");
     }
 
-    const {name, email, password} = req.body;
-
-    // see if users exists
-    
     // Get users Gravatar
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "mm",
+    });
 
     // Encrypt password
 
     // Return jsonweb token
-
-    res.send("user route");
   }
 );
 module.exports = router;
