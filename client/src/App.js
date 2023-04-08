537affebc70a4296ffed48177cfc0c25fc521feb
Neha Singh <neha.singh931126@gmail.com>
2023-04-08 12:55:36 +0200

    Added action and action types and reducer for the alet

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,26 @@
 import { Fragment } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store/store";
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Landing />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-      </Routes>
-    </Fragment>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Provider>
   );
 };
 
